3e6e12c383ebb8ebbadf8df195a1171beabdbe79
Shaun Ryan <shaun_chiburi@hotmail.com>
2024-03-17 12:39:54 +0000

    init

--- contoso_retail_dw/src/etl/table_config.py
+++ contoso_retail_dw/src/etl/table_config.py
@@ -8,578 +8,193 @@ PROJECT = "contoso_retail_dw"
 def tables():
 
   return {
-    "hr_department": {
+    "dim_account": {
       "project": PROJECT,
-      "filename": "humanresources_department",
+      "filename": "dim_account",
       "default": AutoloaderTable,
       "primary_keys": [
-        "department_id"
       ]
     },
-    "hr_employee": {
+    "dim_channel": {
       "project": PROJECT,
-      "filename": "humanresources_employee",
+      "filename": "dim_channel",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_ID"
       ]
     },
-    "hr_employee_department_history": {
+    "dim_account": {
       "project": PROJECT,
-      "filename": "humanresources_employeedepartmenthistory",
+      "filename": "dim_account",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "department_id",
-        "shift_id",
-        "start_date"
       ]
     },
-    "hr_employee_pay_history": {
+    "dim_channel": {
       "project": PROJECT,
-      "filename": "humanresources_employeepayhistory",
+      "filename": "dim_channel",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "rate_change_date"
       ]
     },
-    # "hr_job_candidate": {
-    #   "project": PROJECT,
-    #   "filename": "humanresources_jobcandidate",
-    #   "default": AutoloaderTable,
-    #   "primary_keys": [
-    #     "job_candidate_date"
-    #   ]
-    # },
-    "hr_shift": {
+    "dim_currency": {
       "project": PROJECT,
-      "filename": "humanresources_shift",
+      "filename": "dim_currency",
       "default": AutoloaderTable,
       "primary_keys": [
-        "shift_id"
       ]
     },
-    "person_address": {
+    "dim_customer": {
       "project": PROJECT,
-      "filename": "person__vaddress",
+      "filename": "dim_customer",
       "default": AutoloaderTable,
       "primary_keys": [
-        "address_id"
       ]
     },
-    "person_address_type": {
+    "dim_date": {
       "project": PROJECT,
-      "filename": "person_addresstype",
+      "filename": "dim_date",
       "default": AutoloaderTable,
       "primary_keys": [
-        "address_type_id"
       ]
     },
-    "person_business_entity": {
+    "dim_employee": {
       "project": PROJECT,
-      "filename": "person_businessentity",
+      "filename": "dim_employee",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id"
       ]
     },
-    "person_business_entity_address": {
+    "dim_entity": {
       "project": PROJECT,
-      "filename": "person_businessentityaddress",
+      "filename": "dim_entity",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "address_id",
-        "address_type_id"
       ]
     },
-    "person_business_entity_contact": {
+    "dim_geography": {
       "project": PROJECT,
-      "filename": "person_businessentitycontact",
+      "filename": "dim_geography",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "person_id",
-        "contact_type_id"
       ]
     },
-    "person_contact_type": {
+    "dim_machine": {
       "project": PROJECT,
-      "filename": "person_contacttype",
+      "filename": "dim_machine",
       "default": AutoloaderTable,
       "primary_keys": [
-        "contact_type_id"
       ]
     },
-    "person_country_region": {
+    "dim_outage": {
       "project": PROJECT,
-      "filename": "person_countryregion",
+      "filename": "dim_outage",
       "default": AutoloaderTable,
       "primary_keys": [
-        "country_region_code"
       ]
     },
-    "person_email_address": {
+    "dim_product": {
       "project": PROJECT,
-      "filename": "person_emailaddress",
+      "filename": "dim_product",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "email_address_id"
       ]
     },
-    "person_password": {
+    "dim_product_category": {
       "project": PROJECT,
-      "filename": "person_password",
+      "filename": "dim_product_category",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id"
       ]
     },
-    "person_person": {
+    "dim_product_sub_category": {
       "project": PROJECT,
-      "filename": "person_person",
+      "filename": "dim_product_sub_category",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id"
       ]
     },
-    "person_person_phone": {
+    "dim_promotion": {
       "project": PROJECT,
-      "filename": "person_personphone",
+      "filename": "dim_promotion",
       "default": AutoloaderTable,
       "primary_keys": [
-        "business_entity_id",
-        "phone_number",
-        "phone_number_type_id"
       ]
     },
-    "person_phone_number_type": {
+    "dim_sales_territory": {
       "project": PROJECT,
-      "filename": "person_phonenumbertype",
+      "filename": "dim_sales_territory",
       "default": AutoloaderTable,
       "primary_keys": [
-        "phone_number_type_id"
       ]
     },
-    "person_state_province": {
+    "dim_scenario": {
       "project": PROJECT,
-      "filename": "person_stateprovince",
+      "filename": "dim_scenario",
       "default": AutoloaderTable,
       "primary_keys": [
-        "state_province_id"
       ]
     },
-    "production_bill_of_materials": {
+    "dim_store": {
       "project": PROJECT,
-      "filename": "production_billofmaterials",
+      "filename": "dim_store",
       "default": AutoloaderTable,
       "primary_keys": [
-        "bill_of_materials_id"
       ]
     },
-    "production_culture": {
+    "fact_exchange_rate": {
       "project": PROJECT,
-      "filename": "production_culture",
+      "filename": "fact_exchange_rate",
       "default": AutoloaderTable,
       "primary_keys": [
-        "culture_id"
       ]
     },
-    # "production_document": {
-    #   "project": PROJECT,
-    #   "filename": "production__vdocument",
-    #   "default": AutoloaderTable,
-    #   "primary_keys": [
-    #     "document_node"
-    #   ]
-    # },
-    "production_illustration": {
+    "fact_inventory": {
       "project": PROJECT,
-      "filename": "production_illustration",
+      "filename": "fact_inventory",
       "default": AutoloaderTable,
       "primary_keys": [
-        "illustration_id"
       ]
     },
-    "production_location": {
+    "fact_it_machine": {
       "project": PROJECT,
-      "filename": "production_location",
+      "filename": "fact_it_machine",
       "default": AutoloaderTable,
       "primary_keys": [
-        "location_id"
       ]
     },
-    "production_product": {
+    "fact_it_sla": {
       "project": PROJECT,
-      "filename": "production_product",
+      "filename": "fact_it_sla",
       "default": AutoloaderTable,
       "primary_keys": [
-        "product_id"
       ]
     },
-    "production_product_category": {
+    "fact_online_sales": {
       "project": PROJECT,
-      "filename": "production_productcategory",
+      "filename": "fact_online_sales",
       "default": AutoloaderTable,
       "primary_keys": [
-        "product_category_id"
       ]
     },
-    "production_product_cost_history": {
+    "fact_sales": {
       "project": PROJECT,
-      "filename": "production_productcosthistory",
+      "filename": "fact_sales",
       "default": AutoloaderTable,
       "primary_keys": [
-        "product_id",
-        "start_date"
       ]
     },
-    "production_product_description": {
+    "fact_sales_quota": {
       "project": PROJECT,
-      "filename": "production_productdescription",
+      "filename": "fact_sales_quota",
       "default": AutoloaderTable,
       "primary_keys": [
-        "product_description_id"
       ]
     },
-    "production_product_document": {
+    "fact_strategy_plan": {
       "project": PROJECT,
-      "filename": "production__vproductdocument",
+      "filename": "fact_strategy_plan",
       "default": AutoloaderTable,
       "primary_keys": [
-        "product_id",
-        "document_node"
       ]
-    },
-    "production_product_inventory": {
-      "project": PROJECT,
-      "filename": "production_productinventory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_id",
-        "location_id"
-      ]
-    },
-    "production_product_list_price_history": {
-      "project": PROJECT,
-      "filename": "production_productlistpricehistory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_id",
-        "start_date"
-      ]
-    },
-    # "production_product_model": {
-    #   "project": PROJECT,
-    #   "filename": "production_productmodel",
-    #   "default": AutoloaderTable,
-    #   "primary_keys": [
-    #     "product_model_id"
-    #   ]
-    # },
-    "production_product_model_illustration": {
-      "project": PROJECT,
-      "filename": "production_productmodelillustration",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_model_id",
-        "illustration_id"
-      ]
-    },
-    "production_product_model_product_description_culture": {
-      "project": PROJECT,
-      "filename": "production_productmodelproductdescriptionculture",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_model_id",
-        "product_description_id",
-        "culture_id"
-      ]
-    },
-    "production_product_photo": {
-      "project": PROJECT,
-      "filename": "production_productphoto",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_photo_id"
-      ]
-    },
-    "production_product_product_photo": {
-      "project": PROJECT,
-      "filename": "production_productproductphoto",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_id",
-        "product_photo_id"
-      ]
-    },
-    # "production_product_review": {
-    #   "project": PROJECT,
-    #   "filename": "production_productreview",
-    #   "default": AutoloaderTable,
-    #   "primary_keys": [
-    #     "product_review_id"
-    #   ]
-    # },
-    "production_product_subcategory": {
-      "project": PROJECT,
-      "filename": "production_productsubcategory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "product_subcategory_id"
-      ]
-    },
-    "production_scrap_reason": {
-      "project": PROJECT,
-      "filename": "production_scrapreason",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "scrap_reason_id"
-      ]
-    },
-    "production_transaction_history": {
-      "project": PROJECT,
-      "filename": "production_transactionhistory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "transaction_id"
-      ]
-    },
-    "production_transaction_history_archive": {
-      "project": PROJECT,
-      "filename": "production_transactionhistoryarchive",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "transaction_id"
-      ]
-    },
-    "production_unit_measure": {
-      "project": PROJECT,
-      "filename": "production_unitmeasure",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "unit_measure_code"
-      ]
-    },
-    "production_work_order": {
-      "project": PROJECT,
-      "filename": "production_workorder",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "work_order_id"
-      ]
-    },
-    "production_work_order_routing": {
-      "project": PROJECT,
-      "filename": "production_workorderrouting",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "work_order_id",
-        "product_id",
-        "operation_sequence"
-      ]
-    },
-    "purchasing_product_vendor": {
-      "project": PROJECT,
-      "filename": "purchasing_productvendor",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id"
-      ]
-    },
-    "purchasing_purchase_order_detail": {
-      "project": PROJECT,
-      "filename": "purchasing_purchaseorderdetail",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "purchase_order_id",
-        "purchase_order_detail_id"
-      ]
-    },
-    "purchasing_purchase_order_header": {
-      "project": PROJECT,
-      "filename": "purchasing_purchaseorderheader",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "purchase_order_id"
-      ]
-    },
-    "purchasing_ship_method": {
-      "project": PROJECT,
-      "filename": "purchasing_shipmethod",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "ship_method_id"
-      ]
-    },
-    "purchasing_vendor": {
-      "project": PROJECT,
-      "filename": "purchasing_vendor",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id"
-      ]
-    },
-    "sales_country_region_currency": {
-      "project": PROJECT,
-      "filename": "sales_countryregioncurrency",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "country_region_code",
-        "currency_code"
-      ]
-    },
-    "sales_credit_card": {
-      "project": PROJECT,
-      "filename": "sales_creditcard",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "credit_card_id"
-      ]
-    },
-    "sales_currency": {
-      "project": PROJECT,
-      "filename": "sales_currency",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "currency_code"
-      ]
-    },
-    "sales_currency_rate": {
-      "project": PROJECT,
-      "filename": "sales_currencyrate",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "currency_rate_id"
-      ]
-    },
-    "sales_customer": {
-      "project": PROJECT,
-      "filename": "sales_customer",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "customer_id"
-      ]
-    },
-    "sales_person_credit_card": {
-      "project": PROJECT,
-      "filename": "sales_personcreditcard",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id",
-        "credit_card_id"
-      ]
-    },
-    "sales_sales_order_detail": {
-      "project": PROJECT,
-      "filename": "sales_salesorderdetail",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "sales_order_id",
-        "sales_order_detail_id"
-      ]
-    },
-    "sales_sales_order_header": {
-      "project": PROJECT,
-      "filename": "sales_salesorderheader",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "sales_order_id"
-      ]
-    },
-    "sales_sales_order_header_sales_reason": {
-      "project": PROJECT,
-      "filename": "sales_salesorderheadersalesreason",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "sales_order_id",
-        "sales_reason_id"
-      ]
-    },
-    "sales_sales_person": {
-      "project": PROJECT,
-      "filename": "sales_salesperson",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id"
-      ]
-    },
-    "sales_sales_person_quota_history": {
-      "project": PROJECT,
-      "filename": "sales_salespersonquotahistory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id",
-        "quota_date"
-      ]
-    },
-    "sales_sales_reason": {
-      "project": PROJECT,
-      "filename": "sales_salesreason",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "sales_reason_id"
-      ]
-    },
-    "sales_sales_tax_rate": {
-      "project": PROJECT,
-      "filename": "sales_salestaxrate",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "sales_tax_rate_id"
-      ]
-    },
-    "sales_sales_territory": {
-      "project": PROJECT,
-      "filename": "sales_salesterritory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "territory_id"
-      ]
-    },
-    "sales_sales_territory_history": {
-      "project": PROJECT,
-      "filename": "sales_salesterritoryhistory",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id",
-        "territory_id",
-        "start_date"
-      ]
-    },
-    "sales_shopping_cart_item": {
-      "project": PROJECT,
-      "filename": "sales_shoppingcartitem",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "shopping_cart_item_id"
-      ]
-    },
-    "sales_special_offer": {
-      "project": PROJECT,
-      "filename": "sales_specialoffer",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "special_offer_id"
-      ]
-    },
-    "sales_special_offer_product": {
-      "project": PROJECT,
-      "filename": "sales_specialofferproduct",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "special_offer_id",
-        "product_id"
-      ]
-    },
-    "sales_store": {
-      "project": PROJECT,
-      "filename": "sales_store",
-      "default": AutoloaderTable,
-      "primary_keys": [
-        "business_entity_id"
-      ]
-    },
+    }
   }
